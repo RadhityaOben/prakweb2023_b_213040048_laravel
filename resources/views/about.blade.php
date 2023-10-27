60a7b3047dd5079d56579cadd5f56da21d4b7d89
MineFrostID <obenunity56@gmail.com>
2023-10-27 22:32:52 +0700

    Video #3

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Radit Blog | About</title>
+</head>
+
+<body>
+    <h1>Halaman About</h1>
+    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200" class="rounded-circle shadow">
+    <h3><?= $name; ?></h3>
+    <p><?= $email; ?></p>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
