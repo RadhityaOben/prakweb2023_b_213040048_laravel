e9ca526ec2f7c278679e304a8db9663e0e1f42b3
MineFrostID <obenunity56@gmail.com>
2023-11-02 16:52:30 +0700

    Video #8

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
@@ -34,3 +35,18 @@ Route::get('/posts', [PostController::class, 'index']);
 
 // halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
