21588c5fffe736f75ea599580548f545300a6298
MineFrostID <obenunity56@gmail.com>
2023-11-17 21:05:12 +0700

    Video #14

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session; // Add this line
 
 class RegisterController extends Controller
 {
@@ -12,5 +15,21 @@ class RegisterController extends Controller
             "title" => "Register",
             "active" => "register"
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validData = $request->validate([
+            'name' => 'required|max:255|min:3',
+            'username' => 'required|max:255|min:3|unique:users',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:8|max:255'
+        ]);
+
+        $validData['password'] = Hash::make($validData['password']);
+
+        User::create($validData);
+
+        return redirect('/login')->with('success', 'Registration successful!');
     }
 }
