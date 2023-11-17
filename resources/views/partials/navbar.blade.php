54d02c64bb1a28fa0b70d779e05b8f4043539724
MineFrostID <obenunity56@gmail.com>
2023-11-17 20:33:16 +0700

    Video #14

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,6 +19,11 @@
             <a class="nav-link {{ ($active === "Categories") ? 'active' : '' }}" href="/categories">Categories</a>
           </li>
         </ul>
+        <ul class="navbar-nav ms-auto">
+            <li>
+                <a class="nav-link" href="/login"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+            </li>
+        </ul>
       </div>
     </div>
   </nav>
